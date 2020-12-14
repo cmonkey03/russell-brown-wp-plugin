c9f32e9cba7db6629dc9b9bd6398b6e27ee85d96
Russell Brown <russellpbrown@gmail.com>
2020-12-13 19:21:30 -0500

    Added save functionality to short title input

--- inc/short-description-hooks.php
+++ inc/short-description-hooks.php
@@ -2,6 +2,9 @@
 /**
  * Add the Short Title input filed to Metabox
  * 
+ * The Metabox styling will not matche Gutenberg
+ * See: https://github.com/WordPress/gutenberg/issues/12101
+ * 
  * @param object $post    The post
  * @param array  $metabox The metabox arguments
  */
@@ -42,3 +45,39 @@ function Add_Short_title()
 }
 
 add_action('add_meta_boxes', 'Add_Short_title');
+
+/**
+ * Callback to handle Short Title on save
+ * 
+ * @param int    $post_id The ID for the Post
+ * @param object $post    The Post
+ */
+function Add_Save_Short_Title_meta( int $post_id, object $post )
+{
+    $meta_key = 'article_short_title';
+    $nonce = isset($_POST[$meta_key . '_nonce']) ?
+        sanitize_html_class($_POST[$meta_key . '_nonce']) : '';
+
+    if (!wp_verify_nonce($nonce, basename(__FILE__))) {
+        return $post_id;
+    }
+
+    $post_type = get_post_type_object($post->post_type);
+
+    if (!current_user_can($post_type->cap->edit_post, $post_id)) {
+        return $post_id;
+    }
+
+    $new_meta_value = isset($_POST[$meta_key]) ?
+        sanitize_html_class($_POST[$meta_key]) : '';
+    $meta_value = get_post_meta($post_id, $meta_key, true);
+
+    if ($new_meta_value && '' === $meta_value) {
+        add_post_meta($post_id, $meta_key, $new_meta_value, true);
+    } elseif ($new_meta_value && $new_meta_value !== $meta_value) {
+        update_post_meta($post_id, $meta_key, $new_meta_value);
+    } elseif ('' === $new_meta_value && $meta_value) {
+        delete_post_meta($post_id, $meta_key, $meta_value);
+    }   
+}
+add_action('save_post', 'Add_Save_Short_Title_meta', 10, 2);
